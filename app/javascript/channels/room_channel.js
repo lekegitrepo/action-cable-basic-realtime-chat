970760639d20d9cc9060c008e69c52e7a548aa57
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-04-29 13:46:06 +0100

    Update messages controller

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -11,7 +11,7 @@ consumer.subscriptions.create("RoomChannel", {
   },
 
   received(data) {
-    console.log("This is the received data", data.content);
+    console.log("This is the received data:", data.content);
     // Called when there's incoming data on the websocket for this channel
   },
 });
